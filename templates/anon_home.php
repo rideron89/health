f8efc31b03d66b70524ab043e93f43c5cc715578
Ron Rider <rideron89@gmail.com>
2013-12-16 11:15:55 -0500

    added feature information to home page for anonymous users

---
+++ templates/anon_home.php
@@ -0,0 +1,59 @@
+        <h1>Start managing your health today!</h1>
+        <h4>Create a free account to start logging your diet and exercise routines now.</h4>
+        <div class="grid_1">&nbsp;</div>
+        <div class="grid_5">
+            <h5>Track Your Routine</h5>
+            <ul>
+                <li>Keep a daily log of your diet and excercise habits.</li>
+                <li>Retrieve weekly and monthly summaries to track your progress.</li>
+                <li>For a more engaging experience, set quotas or goals for yourself.</li>
+            </ul>
+            <div>Example:</div>
+            <div class="summary_item">
+                <div class="sidebox_subheader">Diet Info</div>
+                <div class="sidebox_label">Total</div>
+                <div class="sidebox_value">73,645 cals</div><br />
+                <div class="sidebox_label">Monthly Quota</div>
+                <div class="sidebox_value">10,000 cals</div><br />
+                <div class="sidebox_label">This Month</div>
+                <div class="sidebox_value">7,455 cals</div><br />
+                <div class="sidebox_label">Calories To Quota</div>
+                <div class="sidebox_value">2,545 cals</div><br />
+
+                <div class="graph_title">7-Day Graph</div>
+                <div id="diet_graph" class="graph"></div>
+            </div>
+        </div>
+        <div class="grid_5">
+            <h5>Learn About Your Food</h5>
+            <ul>
+                <li>View nutritional information about your favorite foods!</li>
+                <li>Keep track of what you eat by logging your dieting.</li>
+            </ul>
+            <div>Example:</div>
+            <div class="search_item">
+                <div class="sidebox_subheader">Apple</div>
+                <div class="sidebox_label">Serving Size</div>
+                <div class="sidebox_value">1 (179g)</div><br />
+                <div class="sidebox_label">Calorie Count</div>
+                <div class="sidebox_value">93</div><br />
+                <div class="sidebox_label">Calories From Fat</div>
+                <div class="sidebox_value">3</div><br />
+            </div>
+            <div class="search_item">
+                <div class="sidebox_subheader">Banana</div>
+                <div class="sidebox_label">Serving Size</div>
+                <div class="sidebox_value">1 (118g)</div><br />
+                <div class="sidebox_label">Calorie Count</div>
+                <div class="sidebox_value">105</div><br />
+                <div class="sidebox_label">Calories From Fat</div>
+                <div class="sidebox_value">4</div><br />
+            </div>
+        </div>
+        <div class="grid_1">&nbsp;</div>
+        <div class="clear"></div>
+        <div class="grid_12" id="home_create_account_div">
+            <div id="anon_create_account_link">
+                <a href="login.php">Create Account / Login</a>
+            </div>
+        </div>
